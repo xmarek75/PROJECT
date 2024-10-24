00592a23ee028da12217e8e4fa28bf2e1eb16af1
Pavel Marek <80002330+xmarek75@users.noreply.github.com>
2024-10-24 11:23:44 +0200

    Add files via upload

--- whisper_test.py
+++ whisper_test.py
@@ -1,7 +1,23 @@
 import whisper
 
-model=whisper.load_model("base")
+# Načtení modelu
+model = whisper.load_model("base")
 
+# Transkripce souboru
 result = model.transcribe("audio_sample.mp3")
+
+
+def simple_hours(seconds: float) -> float:
+    minutes = seconds // 60  # Celé minuty
+    seconds = seconds % 60     # Zbytek sekund
+    return minutes, seconds
+# Výpis transkripce s časovými značkami
 print("===============================================================================================")
-print(result["text"])
+for segment in result["segments"]:
+    start_time = segment["start"]  # Čas začátku segmentu
+    end_time = segment["end"]      # Čas konce segmentu
+    text = segment["text"]         # Text segmentu
+    start_minutes, start_seconds = simple_hours(start_time)#prepis na minuty a vteriny
+    end_minutes, end_seconds = simple_hours(end_time)
+
+    print(f"[{start_minutes:.0f}:{start_seconds:.2f} - {end_minutes:.0f}:{end_seconds:.2f}] {text}")
